531d905f9edceb5d3e5d2696831ebfea775b7a5a
Ilya <Dragoilya@yandex.ru>
2023-01-25 16:13:31 +0300

    Added removed confidentional information

--- project/settings.py
+++ project/settings.py
@@ -1,43 +1,44 @@
 import os
+from dotenv import dotenv_values
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'ENGINE': dotenv_values(".env")["DATABASES_DEFAULT_ENGINE"],
+        'HOST': dotenv_values(".env")["DATABASES_DEFAULT_HOST"],
+        'PORT': dotenv_values(".env")["DATABASES_DEFAULT_PORT"],
+        'NAME': dotenv_values(".env")["DATABASES_DEFAULT_NAME"],
+        'USER': dotenv_values(".env")["DATABASES_DEFAULT_USER"],
+        'PASSWORD': dotenv_values(".env")["DATABASES_DEFAULT_PASSWORD"],
     }
 }
 
-INSTALLED_APPS = ['datacenter']
+INSTALLED_APPS = [dotenv_values(".env")["INSTALLED_APPS"]]
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = dotenv_values(".env")["SECRET_KEY"]
 
-DEBUG = True
+DEBUG = dotenv_values(".env")["DEBUG"]=='True'
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = dotenv_values(".env")["ROOT_URLCONF"]
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [dotenv_values(".env")["ALLOWED_HOSTS"]]
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
+        'BACKEND': dotenv_values(".env")["TEMPLATES_BACKEND"],
+        'DIRS': [os.path.join(BASE_DIR, dotenv_values(".env")["TEMPLATES_DIRS"])],
+        'APP_DIRS': dotenv_values(".env")["TEMPLATES_APP_DIRS"]=='True',
     },
 ]
 
 
-USE_L10N = True
+USE_L10N = dotenv_values(".env")["USE_L10N"]=='True'
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = dotenv_values(".env")["LANGUAGE_CODE"]
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = dotenv_values(".env")["TIME_ZONE"]
 
-USE_TZ = True
+USE_TZ = dotenv_values(".env")["USE_TZ"]=='True'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = dotenv_values(".env")["DEFAULT_AUTO_FIELD"]
